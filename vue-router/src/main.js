5e868604f497e67c7699efbe539b997bf322c701
darrenchin <darrenschoolemail@gmail.com>
2022-03-05 18:33:43 -0500

    s
    s

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
---
+++ vue-router/src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {},
+
+  created: function () {
+    console.log("created");
+ },
+ };
+</script>
