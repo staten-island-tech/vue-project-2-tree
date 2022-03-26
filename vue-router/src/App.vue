030515f54127ac117a1db5ddaa8a4d1f5e09efaa
darrenchin <darrenschoolemail@gmail.com>
2022-03-26 18:43:52 -0400

    s
    ss

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link> 
     <router-link to="/about">About</router-link>
     <router-link to="/SignupView">Sign Up</router-link>
   </nav>
