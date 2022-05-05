f141266b66b4eac16b34b8ae6402d511b3888252
darrenchin <darrenschoolemail@gmail.com>
2022-05-05 13:03:56 -0400

    thank gooodnesss
    yaya

--- vue-router/src/firebase/config.js
+++ vue-router/src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADtiPFcL7HQbsJUvj9kXplqU8k02Dv6WU",
-  authDomain: "fir-vuex-proje.firebaseapp.com",
-  projectId: "fir-vuex-proje",
-  storageBucket: "fir-vuex-proje.appspot.com",
-  messagingSenderId: "161461179052",
-  appId: "1:161461179052:web:1233d3cfac57b758b58509",
+  apiKey: "AIzaSyCnR_y2kkwqB-m-R-5hfRptxuU535lMc0k",
+  authDomain: "fir-2-vuex-project.firebaseapp.com",
+  projectId: "fir-2-vuex-project",
+  storageBucket: "fir-2-vuex-project.appspot.com",
+  messagingSenderId: "1072061725777",
+  appId: "1:1072061725777:web:d6e0ea9520d7eeef450c70",
 };
 
 const app = initializeApp(firebaseConfig);
