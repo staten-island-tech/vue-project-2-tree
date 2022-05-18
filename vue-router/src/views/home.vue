db442efcacad445e37ecd865a35c9a782ab974e0
darrenchin <darrenschoolemail@gmail.com>
2022-05-18 02:19:20 -0400

    s
    s

--- vue-router/src/views/home.vue
+++ vue-router/src/views/home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
   <div v-for="blog in blogs" :key="blog.id">
-      <div class="blog">
-        <h3>{{ blog.title }}</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aspernatur consectetur doloremque sunt ducimus enim iure animi fugit nulla et! Perferendis autem deleniti quo eum corrupti reiciendis voluptatem ab ducimus?</p>
-        <div class="icons">
-          <span>upvote or downvote this article: </span>
-          <span class="material-icons">thumb_up</span>
-          <span class="material-icons">thumb_down</span>
-        </div>
-      </div>
+       <Card 
+    v-for="(item,index) in items" 
+    :key="item.name"
+    :title="item.name"
+    :price="item.price"
+    :image="item.image"
+    :index="index"
+    :add="carrt"
+    >
+  </Card>
     </div>
   </div>
 </template>
@@ -23,12 +24,20 @@ import { collection, getDocs } from "firebase/firestore";
 export default {
   
   name: 'HomeView',
-  setup() {
-      const blogs = ref([
-        { title: 'Why Coffee is Better than Tea', id: 1 },
+data() {
+  return {
+    selected: "",
+    cart: [],
+    total: 0,
+    items: [
+      { title: 'Why Coffee is Better than Tea', id: 1 },
         { title: '...Then I Took an Arrow in the Knee', id: 2 },
         { title: 'Mario vs Luigi, Ultimate Showdown', id: 3 },
-      ])
+    ]
+  }
+}
+}
+    
       const store = useStore()
       
       console.log(store.state.user)
@@ -36,7 +45,7 @@ export default {
       return { 
         blogs
       }
-  }
-}
+  
+
 
 </script>
