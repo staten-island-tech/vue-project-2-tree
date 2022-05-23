4f2e80f988901c94c9062c6ab6dac70604a8e90d
darrenchin <darrenschoolemail@gmail.com>
2022-05-22 20:28:00 -0400

    card component
    ok

---
+++ vue-router/src/components/UserCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="carr">
+        <h2 class="title">{{ title }}</h2>
+        <img class="image" v-bind:src=image alt="image">
+        <router-link class="add-btn">View Post</router-link>
+    </div>
+    </template>
+
+<script>
+    export default {
+        title:String,
+        image:String,
+    }
+</script>
+
